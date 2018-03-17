4c1b1f6aa35c5497991ac921ed576ff54534b534
Kim Eng Ky <kimeng.ky@metrotransit.org>
2018-03-16 20:14:43 -0500

    connect to google sheet and add saving and debt tab

--- server.R
+++ server.R
@@ -1,29 +1,43 @@
-all_trans <- readRDS("all_transactions.RDS")
-budget <- read_rds("budget.RDS")
+# all_trans <- readRDS("all_transactions.RDS") %>% filter(!grepl("JHU STDNT ACCT SELF SRV", Description) & WorkTrans == 0)
+budget <- readRDS("budget.RDS")
 
 shinyServer(function(input, output, session) {
   observe({
     updateSelectInput(session, 'cat', 
-                      choices = all_trans %>% arrange(NewCat) %>% select(NewCat) %>% 
-                        unique() %>% pull(NewCat), 
+                      choices = daily_spending %>% 
+                        filter(!Category %in% c("Car Loan", "Student Loan", "Saving", "Brokerage", "Income")) %>% 
+                        arrange(Category) %>% select(Category) %>% 
+                        unique() %>% pull(Category), 
                       selected = "Restaurants")
+    updateDateRangeInput(session, "dr_filter", min = min(daily_spending$Date), max = max(daily_spending$Date), 
+                         start = min(daily_spending$Date), end = max(daily_spending$Date))
+    updateCheckboxGroupInput(session, "cat_filter", choices = daily_spending %>% 
+                               filter(!Category %in% c("Car Loan", "Student Loan", "Saving", "Brokerage", "Income")) %>% 
+                               arrange(Category) %>% select(Category) %>% 
+                               unique() %>% pull(Category), 
+                             selected = daily_spending %>% 
+                               filter(!Category %in% c("Car Loan", "Student Loan", "Saving", "Brokerage", "Income")) %>% 
+                               arrange(Category) %>% select(Category) %>% 
+                               unique() %>% pull(Category))
+    mons <- daily_spending %>% arrange(Month) %>% select(Month) %>% 
+      unique() %>% pull(Month)
+    updateSelectInput(session, 'mon', choices = mons)
+    updateSelectInput(session, 'mon_saving', choices = mons)
+    updateSelectInput(session, 'mon_debt', choices = mons)
   })
   
-  observe({
-    updateSelectInput(session, 'mon', 
-                      choices = all_trans %>% arrange(Month) %>% select(Month) %>% 
-                        unique() %>% pull(Month))
-  })
-  
+  # by cat ----
   curr_dat <- reactive({
-    req(input$cat)
-    curr_dat <- all_trans %>% filter(NewCat %in% input$cat)
+    req(input$cat, input$dr_filter)
+    curr_dat <- daily_spending %>% filter(Category %in% input$cat & Date >= input$dr_filter[1] & Date <= input$dr_filter[2])
     return(curr_dat)
   })
   
   plot_dat <- reactive({
-    req(input$mode == "Total")
-    all_cats <- all_trans %>% group_by(Month) %>% summarise(Total = sum(Amount))
+    req(input$cat)
+    all_cats <- daily_spending %>% 
+      filter(Date >= input$dr_filter[1] & Date <= input$dr_filter[2]) %>% 
+      group_by(Month) %>% summarise(Total = sum(Amount))
     plot_dat <- curr_dat() %>% group_by(Month) %>% summarise(`Amount Spent` = sum(Amount))
     full_join(plot_dat, all_cats, by = "Month") %>%
       mutate_if(is.numeric, function(x) if_else(is.na(x), 0, x))
@@ -70,16 +84,16 @@ shinyServer(function(input, output, session) {
   
   output$all <- renderDataTable({
     req(curr_dat())
-    return(curr_dat() %>% select(Date, Description, Amount, Category = NewCat))
+    return(curr_dat())
   })
   
   output$overspend_cat <- renderValueBox({
     req(input$cat)
-    curr_budget <- budget %>% filter(Category %in% input$cat) %>% 
+    curr_budget <- budget %>% filter(Category %in% input$cat & !Category %in% c("Car Loan", "Student Loan", "Saving", "Brokerage", "Income")) %>% 
       summarise(Budget = sum(Budget)) %>% pull(Budget)
     curr_exp <- mean(plot_dat()$`Amount Spent`)
     diff_spending <- curr_budget - curr_exp
-    if (overspending < 0) {
+    if (diff_spending < 0) {
       valueBox(
         paste0("$", prettyNum(round(diff_spending), big.mark = ",")),
         subtitle = "Average Monthly Over-spending",
@@ -99,12 +113,12 @@ shinyServer(function(input, output, session) {
   })
   
   curr_dat_mon <- reactive({
-    req(input$mon) 
-    all_trans %>% 
-      filter(Month == as.Date(input$mon)) %>% 
-      group_by(Category = NewCat) %>% 
+    req(input$mon, input$cat_filter) 
+    daily_spending %>% 
+      filter(Month == as.Date(input$mon) & Category %in% input$cat_filter & !Category %in% c("Income", "Saving", "Student Loan", "Car Loan", "Brokerage")) %>% 
+      group_by(Category = Category) %>% 
       summarise(Amount = sum(Amount)) %>% 
-      full_join(budget, by = "Category") %>% 
+      full_join(budget %>% filter(Category %in% input$cat_filter), by = "Category") %>% 
       mutate_if(is.numeric, function(x) if_else(is.na(x), 0, x))  %>% 
       mutate(pct = round(Amount*100 / sum(Amount), 2), 
              Category2 = sprintf('%s: %s%%', Category, pct))
@@ -122,13 +136,15 @@ shinyServer(function(input, output, session) {
       theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.title = element_blank()) +
       ggtitle(paste0("Total Spending This Month = ", 
                      prettyNum(round(sum(curr_dat$Amount), 0), big.mark = ","),
-                     "; Total Budget = ", prettyNum(round(sum(budget$Budget), 0), big.mark = ",")))
+                     "; Total Budget = ", prettyNum(round(sum(curr_dat$Budget), 0), big.mark = ",")))
   })
   
   output$overspend_mon <- renderValueBox({
     curr_dat <- curr_dat_mon()
     tot_spending <- sum(curr_dat$Amount)
-    tot_budget <- sum(budget$Budget)
+    tot_budget <- budget %>% 
+      filter(!Category %in% c("Car Loan", "Student Loan", "Saving", "Brokerage", "Income")) %>% 
+      pull(Budget) %>% sum()
     diff_spending <- tot_budget - tot_spending
     if (diff_spending < 0) {
       valueBox(
@@ -151,6 +167,141 @@ shinyServer(function(input, output, session) {
   
   output$all_mon <- renderDataTable({
     req(input$mon) 
-    all_trans %>% filter(Month == as.Date(input$mon)) %>% select(Date, Description, Amount, Category = NewCat)
+    daily_spending %>% filter(Month == as.Date(input$mon) & Category %in% input$cat_filter)
+  })
+  # saving -----
+  curr_dat_saving <- reactive({
+    req(input$mon_saving) 
+    daily_spending %>% 
+      filter(Month == as.Date(input$mon_saving) & Category %in% c("Saving", "Brokerage")) %>% 
+      group_by(Category) %>% 
+      summarise(Amount = sum(Amount)) %>% 
+      full_join(budget %>% filter(Category %in% c("Saving", "Brokerage")), by = "Category") %>% 
+      mutate_if(is.numeric, function(x) if_else(is.na(x), 0, x))  %>% 
+      mutate(pct = round(Amount*100 / sum(Amount), 2), 
+             Category2 = sprintf('%s: %s%%', Category, pct))
+  })
+  
+  output$p_saving <- renderPlot({
+    curr_dat <- curr_dat_saving()
+    curr_dat %>% 
+      ggplot(aes(x = Category2)) +
+      geom_bar(aes(y = Amount, fill = Category2), stat = 'identity') +
+      geom_label(aes(y = Amount, label = paste0("$", prettyNum(round(Amount, 0), big.mark = ",")))) +
+      geom_point(aes(y = Budget), color = "#0053A0", size = 3) +
+      labs(x = "") +
+      scale_x_discrete(breaks = curr_dat$Category2, labels = curr_dat$Category) + 
+      theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.title = element_blank()) +
+      ggtitle(paste0("Total Saving This Month = ", 
+                     prettyNum(round(sum(curr_dat$Amount), 0), big.mark = ","),
+                     "; Total Budget = ", prettyNum(round(sum(curr_dat$Budget), 0), big.mark = ",")))
+  })
+  
+  output$net_saving <- renderValueBox({
+    curr_dat <- curr_dat_saving() %>% filter(Category == "Saving")
+    tot_saving <- sum(curr_dat$Amount)
+    tot_budget <- budget %>% filter(Category %in% c("Saving")) %>% pull(Budget) %>% sum()
+    net_saving <- tot_saving - tot_budget
+    if (net_saving < 0) {
+      valueBox(
+        paste0("$", prettyNum(round(net_saving), big.mark = ",")),
+        subtitle = paste0(format(as.Date(input$mon_saving), "%b %Y"), " Saving"),
+        icon = icon("money"),
+        color = 'red', 
+        width = 6
+      )
+    } else {
+      valueBox(
+        paste0("$", prettyNum(round(net_saving), big.mark = ",")),
+        subtitle = paste0(format(as.Date(input$mon_saving), "%b %Y"), " Saving"),
+        icon = icon("money"),
+        color = 'green', 
+        width = 6
+      )
+    }
+  })
+  output$net_brokerage <- renderValueBox({
+    curr_dat <- curr_dat_saving() %>% filter(Category %in% c("Brokerage"))
+    tot_brokerage <- sum(curr_dat$Amount)
+    tot_budget <- budget %>% filter(Category %in% c("Brokerage")) %>% pull(Budget) %>% sum()
+    net_brokerage <- tot_brokerage - tot_budget
+    if (net_brokerage < 0) {
+      valueBox(
+        paste0("$", prettyNum(round(net_brokerage), big.mark = ",")),
+        subtitle = paste0(format(as.Date(input$mon_saving), "%b %Y"), " Brokerage"),
+        icon = icon("money"),
+        color = 'red', 
+        width = 6
+      )
+    } else {
+      valueBox(
+        paste0("$", prettyNum(round(net_brokerage), big.mark = ",")),
+        subtitle = paste0(format(as.Date(input$mon_saving), "%b %Y"), " Brokerage"),
+        icon = icon("money"),
+        color = 'green', 
+        width = 6
+      )
+    }
+  })
+  
+  output$all_saving <- renderDataTable({
+    req(input$mon_saving) 
+    daily_spending %>% filter(Month == as.Date(input$mon_saving) & Category %in% c("Saving", "Brokerage"))
+  })
+  # debt ----
+  curr_dat_debt <- reactive({
+    req(input$mon_debt) 
+    daily_spending %>% 
+      filter(Month == as.Date(input$mon_debt) & Category %in% c("Student Loan", "Car Loan")) %>% 
+      group_by(Category) %>% 
+      summarise(Amount = sum(Amount)) %>% 
+      full_join(budget %>% filter(Category %in% c("Student Loan", "Car Loan")), by = "Category") %>% 
+      mutate_if(is.numeric, function(x) if_else(is.na(x), 0, x))  %>% 
+      mutate(pct = round(Amount*100 / sum(Amount), 2), 
+             Category2 = sprintf('%s: %s%%', Category, pct))
+  })
+  
+  output$p_debt <- renderPlot({
+    curr_dat <- curr_dat_debt()
+    curr_dat %>% 
+      ggplot(aes(x = Category2)) +
+      geom_bar(aes(y = Amount, fill = Category2), stat = 'identity') +
+      geom_label(aes(y = Amount, label = paste0("$", prettyNum(round(Amount, 0), big.mark = ",")))) +
+      geom_point(aes(y = Budget), color = "#0053A0", size = 3) +
+      labs(x = "") +
+      scale_x_discrete(breaks = curr_dat$Category2, labels = curr_dat$Category) + 
+      theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.title = element_blank()) +
+      ggtitle(paste0("Total Paid This Month = ", 
+                     prettyNum(round(sum(curr_dat$Amount), 0), big.mark = ","),
+                     "; Total Budget = ", prettyNum(round(sum(curr_dat$Budget), 0), big.mark = ",")))
+  })
+  
+  output$net_debt <- renderValueBox({
+    curr_dat <- curr_dat_debt()
+    tot_debt <- sum(curr_dat$Amount)
+    tot_budget <- budget %>% filter(Category %in% c("Student Loan", "Car Loan")) %>% pull(Budget) %>% sum()
+    net_debt <- tot_debt - tot_budget
+    if (net_debt < 0) {
+      valueBox(
+        paste0("$", prettyNum(round(net_debt), big.mark = ",")),
+        subtitle = paste0(format(as.Date(input$mon_debt), "%b %Y"), " Underpaid"),
+        icon = icon("money"),
+        color = 'red', 
+        width = 6
+      )
+    } else {
+      valueBox(
+        paste0("$", prettyNum(round(net_debt), big.mark = ",")),
+        subtitle = paste0(format(as.Date(input$mon_debt), "%b %Y"), " Overpaid"),
+        icon = icon("money"),
+        color = 'green', 
+        width = 6
+      )
+    }
+  })
+  
+  output$all_debt <- renderDataTable({
+    req(input$mon_debt) 
+    daily_spending %>% filter(Month == as.Date(input$mon_debt) & Category %in% c("Student Loan", "Car Loan"))
   })
 })
--- ui.R
+++ ui.R
@@ -10,8 +10,10 @@ shinyUI(
     dashboardHeader(title = "Budget Analysis"), 
     dashboardSidebar(
       sidebarMenu(id = 'menu',
-        menuItem(text = "Stats by Category", tabName = "sum"),
-        menuItem(text = "Stats by Month", tabName = 'tot')
+        menuItem(text = "Expenses by Category", tabName = "sum"),
+        menuItem(text = "Expenses by Month", tabName = 'tot'),
+        menuItem(text = "Income and Saving", tabName = "saving"),
+        menuItem(text = "Debt", tabName = "debt")
       ), 
       tags$hr(),
       conditionalPanel(
@@ -19,13 +21,25 @@ shinyUI(
         box(width = 12, background = 'blue', 
             selectInput('cat', "Select Category", multiple = T, 
                         choices = c()), 
-            radioButtons('mode', NULL, choices = c("Total", "Average"))
+            radioButtons('mode', NULL, choices = c("Total", "Average")), 
+            dateRangeInput('dr_filter', "Select Date Range")
         )
       ),
       conditionalPanel(
         condition = "input.menu == 'tot'",
         box(width = 12, background = 'blue', 
-            selectInput('mon', "Select Month", choices = c()))
+            selectInput('mon', "Select Month", choices = c()),
+            checkboxGroupInput('cat_filter', "Select Categories", choices = c()))
+      ),
+      conditionalPanel(
+        condition = "input.menu == 'saving'",
+        box(width = 12, background = 'blue', 
+            selectInput('mon_saving', "Select Month", choices = c()))
+      ),
+      conditionalPanel(
+        condition = "input.menu == 'debt'",
+        box(width = 12, background = 'blue', 
+            selectInput('mon_debt', "Select Month", choices = c()))
       )
       ), 
     dashboardBody(
@@ -53,6 +67,23 @@ shinyUI(
               plotOutput('p_by_mon', height = '500px')), 
           box(width = 12, solidHeader = T, title = "All Transactions", status = "primary", 
               dataTableOutput('all_mon'))
+        ),
+        tabItem(
+          'saving',
+          fluidRow(column(6, valueBoxOutput('net_saving')), 
+                   column(6, valueBoxOutput('net_brokerage'))),
+          box(width = 12, solidHeader = T, title = "Income and Saving", status = "primary", 
+              plotOutput('p_saving', height = "500px")),
+          box(width = 12, solidHeader = T, title = "Transactions", status = "primary", 
+              dataTableOutput('all_saving'))
+        ),
+        tabItem(
+          'debt', 
+          valueBoxOutput("net_debt"),
+          box(width = 12, solidHeader = T, title = "Debt", status = "primary",
+              plotOutput('p_debt', height = "500px")),
+          box(width = 12, solidHeader = T, title = "Transactions", status = "primary", 
+              dataTableOutput("all_debt"))
         )
       )
     )
